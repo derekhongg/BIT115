6ae9e79759ca80202425937bb38fb3d9c07c6037
derekhongg <hongderek@gmail.com>
2023-10-24 18:56:42 -0700

    new updates

--- Week 5/10242023/CustomerPlans.java
+++ Week 5/10242023/CustomerPlans.java
@@ -1,42 +1,49 @@
+// Derek Hong
+// 10.24.2023
+// CustomerPlans.java
+// Purpose: This code determines the cost for minutes depending if you're a residential or commerical customer
+
+
+
 import java.util.Scanner;
 public class CustomerPlans{
 	public static void main(String[] args){
-		int minutes;
-		double cost = 0.0;
-		char customerType;
+		int minutes; // sets up the variable for minutes
+		double cost = 0.0; // sets up variables for cost
+		char customerType; // sets up variable fo customer type
 		
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in); //sets up scanner for input
 		
 		System.out.println("Are you a residential or commercial customer? ");
 		System.out.println("type res for residential or com for commercial");
-		customerType = input.next().toLowerCase().charAt(0);
+		customerType = input.next().toLowerCase().charAt(0); // customer type will be the first letter of the input
 		if(customerType == 'r' || customerType == 'c'){
 			System.out.println("Enter the number of minutes used for the week");
-			minutes = input.nextInt();
+			minutes = input.nextInt(); // will ask you to input the numbers of minutes used
 			
-			int extalMinR = minutes - 60;
-			int extralMinC = minutes - 300;
+			int extalMinR = minutes - 60;// gets the extra minutes for residentials
+			int extralMinC = minutes - 300; // gets the extra minutes for commercials
 			
-			if (customerType == 'r' && minutes <= 60){
-				cost = 5 ;
+			if (customerType == 'r' && minutes <= 60){ 
+				cost = 5 ; // if customer type is residential and minutes is less than or equal to 60, cost is $5.00
 			} else {
-				cost = 5 + (0.1 * extalMinR);
+				cost = 5 + (0.1 * extalMinR); // else cost will be $5.00 + 10% of extra minutes used
 			}
 		
-			if(customerType == 'c' && minutes <= 300){
-			   cost = 300 * 0.2;
+			if(customerType == 'c' && minutes <= 300) { 
+			   cost = 300 * 0.2; // if customer type is commercial and less than 300 minutes, it will charge a flat rate of $60.00
 			} else if (customerType != 'r') {
-			   cost = (300 * 0.2) + (0.15 * extralMinC);		
+			   cost = (300 * 0.2) + (0.15 * extralMinC);	//if more than 300 minutes are used it'll charge $60.00 + 15% of extra minutes used	
 			}
 			
-			System.out.print("Customer Type: " + customerType);
-			System.out.print(", Total minutes used = " + minutes);
-			System.out.printf(", and your total cost = $%.2f", cost );
+			System.out.print("Customer Type: " + customerType); // print out customer type
+			System.out.print(", Total minutes used = " + minutes); // print out total minutes used
+			System.out.printf(", and your total cost = $%.2f", cost ); // print out total cost up to two decimals
 			//System.out.println(", and your total cost = " + cost);
 			System.out.println();
 	
 		} else {
-		   System.out.println("Error: Invalid input. Try again!!");
+		   System.out.println("Error: Invalid input. Try again!!"); // if customerType doesn't equal r or c, it'll print out an error
 		}
 	}
 }
--- Week 5/10242023/MovieTicket.java
+++ Week 5/10242023/MovieTicket.java
@@ -1,3 +1,8 @@
+// Derek Hong
+// 10.24.2023
+// MovieTicket.java
+// Purpose: To determine movie ticket prices depending on age & county customeris living in
+
 import java.util.Scanner;
 
 public class MovieTicket {
@@ -7,7 +12,7 @@ public class MovieTicket {
       
       int age;
       double price;
-      String county = "";
+      char county;
       
       System.out.println("Please enter your age: ");
       age = cin.nextInt();
@@ -18,22 +23,23 @@ public class MovieTicket {
       };
       
       System.out.println("Please enter what county you live in: ");
-      county = cin.next();
+      county = cin.next().toLowerCase().charAt(0);
       
       
-      if(county.equalsIgnoreCase("King")) {
+      if(county == 'k') {
          price = 30.0;
       } else {
          price = 40.0;
       };
       
-      if(county.equalsIgnoreCase("King") && age < 14 && age >= 5) {
+      if(county == 'k' && age < 14 && age >= 5) {
          price = (30.0 * 0.82);
+         price = price;
       } else if (age < 14) {
-         price = (40.0 * 0.82);
+         price = 40.0 * 0.82;
       }
       
-      if(county.equalsIgnoreCase("King") && age >= 65) {
+      if(county == 'k' && age >= 65) {
          price = (30.0/2);
       } else if(age >= 65) {
          price = (40.0/2);
@@ -43,6 +49,7 @@ public class MovieTicket {
          price = 0.0;
       }
       
-      System.out.println("Your price is: $" + price);
+      System.out.printf("Your price is: $%.2f", price);
+      System.out.println();
    }
 }
